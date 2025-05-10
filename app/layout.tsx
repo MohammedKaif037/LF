0094f4e0dd82cbd0c6bc03faccb80f3d18a34fac
Mohammed Kaif <105605883+MohammedKaif037@users.noreply.github.com>
2025-05-10 22:54:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-
+//Root layout Cobfig
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
