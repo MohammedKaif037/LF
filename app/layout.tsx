f00d2e73be2201674e7726cc6e8f499edf12b626
Mohammed Kaif <105605883+MohammedKaif037@users.noreply.github.com>
2025-05-11 21:04:08 +0530

    Update layout.tsx to add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-//Root layout Cobfig
+//Root
+import { AuthProvider } from "@/components/auth-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -21,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
-          {children}
+          <AuthProvider>{children}</AuthProvider>
         </ThemeProvider>
       </body>
     </html>
